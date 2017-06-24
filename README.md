8ef49d7947a94a834de7562723b1db0017335c39
Victoria Quirante <victoria@limenius.com>
2017-06-24 16:46:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+serialization
+=============
+
+A Symfony project created on June 24, 2017, 4:35 pm.
---
+++ src/AppBundle/Entity/Spaceship.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Spaceship
+ *
+ * @ORM\Table(name="spaceship")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SpaceshipRepository")
+ */
+class Spaceship
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=255)
+     */
+    private $color;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="maxSpeed", type="integer")
+     */
+    private $maxSpeed;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Spaceship
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Spaceship
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set maxSpeed
+     *
+     * @param integer $maxSpeed
+     *
+     * @return Spaceship
+     */
+    public function setMaxSpeed($maxSpeed)
+    {
+        $this->maxSpeed = $maxSpeed;
+
+        return $this;
+    }
+
+    /**
+     * Get maxSpeed
+     *
+     * @return int
+     */
+    public function getMaxSpeed()
+    {
+        return $this->maxSpeed;
+    }
+}
+
